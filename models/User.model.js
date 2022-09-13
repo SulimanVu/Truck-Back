ed0364fbb7df8e3adb3fb39cd532a709619bfba9
Ayub <aub9@mail.ru>
2022-09-13 10:14:20 +0300

    Добавил User model,controller again

--- models/User.model.js
+++ models/User.model.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true
+  },
   phone: String,
   mail: String,
   login: String,
