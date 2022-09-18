7a4f0c55f68f51ab3b9e00ae52e32b5cd6bac035
Waldgang <waldgang@MacBook-Air-salambek.local>
2022-09-18 16:48:52 +0300

    Добавил морган мидлвейр

--- routes/request.route.js
+++ routes/request.route.js
@@ -2,10 +2,11 @@ const {Router} = require('express')
 const router = Router()
 const {request} = require('../controllers/request.controller')
 const authMiddleware = require('../middleware/auth.middleware')
-
+const morgan = require('morgan')
+ç
 router.get('/request', request.getRequests)
-router.post('/request', authMiddleware, request.addRequest)
-router.patch('/request/:id', authMiddleware, request.updateRequest)
-router.delete('/request/:id', authMiddleware, request.deleteRequest)
+router.post('/request',morgan('combined'), authMiddleware, request.addRequest)
+router.patch('/request/:id', morgan('combined'),authMiddleware, request.updateRequest)
+router.delete('/request/:id',morgan('combined'), authMiddleware, request.deleteRequest)
 
 module.exports = router
